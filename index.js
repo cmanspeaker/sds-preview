ce392c31724bd4f963c7f0428963a64bd267cdc3
Michalis <mixalistzikas@gmail.com>
2021-02-25 22:36:38 +0200

    fix

--- index.js
+++ index.js
@@ -5,17 +5,9 @@ fs.renameSync('./schoox/.style.css', './schoox/style.css', function(err) {
 });
 // read the file
 var content = (fs.readFileSync("./schoox/style.css")).toString();
-// print it
 
-var find = '.schoox-';
-var re = new RegExp(find, 'g');
-
-content = content.replace(re, '.cl-');
-
-var find = '.schoox {';
-var re = new RegExp(find, 'g');
-
-content = content.replace(re, '.cl {');
+var find = /\.schoox/gm;
+var content = content.replace(find, '.cl');
 
 fs.writeFile("./schoox/cl-icons-font.css", content, function(err) {
     if(err) {
