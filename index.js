cc8cd931172b4aedac7f30c25b501d98220cb8b6
Michalis <mixalistzikas@gmail.com>
2021-04-09 10:52:02 +0300

    fix

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ fs.renameSync('./schoox/.style.css', './schoox/style.css', function(err) {
 // read the file
 var content = (fs.readFileSync("./schoox/style.css")).toString();
 
-var find = /\.schoox/gm;
-var content = content.replace(find, '.cl');
+const regex = /\.schoox-(.*?):/gm;
+let m;
+let icons = [];
 
-fs.writeFile("./schoox/cl-icons-font.css", content, function(err) {
+while ((m = regex.exec(content)) !== null) {
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+    icons.push(m[1]);
+}
+fs.writeFile("./schoox/icons.json", JSON.stringify(icons), function(err) {
     if(err) {
         return console.log(err);
     }
     console.log("The file was saved!");
 });
+
+// var find = /\.schoox/gm;
+// var content = content.replace(find, '.cl');
+//
+// fs.writeFile("./schoox/cl-icons-font.css", content, function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+//     console.log("The file was saved!");
+// });
